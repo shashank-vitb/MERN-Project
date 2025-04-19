f98ea821a7ca79fc4b5667896e443e73950915a3
shashank-vitb <shashank2022@vitbhopal.ac.in>
2025-04-19 22:15:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/api/songs', songRoutes);
 app.use('/api/playlists', playlistRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
